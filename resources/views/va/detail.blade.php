f095df0a3369e22f5cd9287219c1759258277df7
Maspri <priyono.arif@gmail.com>
2023-08-09 12:48:15 +0700

    fix: trx history table

--- resources/views/va/detail.blade.php
+++ resources/views/va/detail.blade.php
@@ -1,47 +1,51 @@
 @extends('dashboard.base')
 
 @section('content')
-<x-containers.container size="6">
-  <x-containers.card>
-    <x-slot name="title">{{ $va->number }}</x-slot>
-    <table class="table">
-      <tr>
-        <td>User Name</td>
-        <td>: {{ $va->user->name }}</td>
-      </tr>
-      <tr>
-        <td>VA Number</td>
-        <td>: {{ $va->number }}</td>
-      </tr>
-    </table>
-    <x-forms.button href="{{ route('va.index') }}" preset="default">{{ __('Back') }}</x-forms.button>
-  </x-containers.card>
-</x-containers.container>
-<x-containers.container size="6">
-  <x-containers.card>
-    <x-slot name="title">History Transaksi</x-slot>
-    <table class="table">
-      <thead>
-        <th>Waktu</th>
-        <th>Total Pembayaran</th>
-      </thead>
-      <tbody>
-        @if (!isset($payments) || sizeof($payments) <= 0)
+<x-containers.container size="12">
+  <div class="row">
+    <div class="col-md-6">
+      <x-containers.card>
+        <x-slot name="title">{{ $va->number }}</x-slot>
+        <table class="table">
           <tr>
-            <td colspan="2">Belum ada history</td>
+            <td>User Name</td>
+            <td>: {{ $va->user->name }}</td>
           </tr>
-        @else
-          @foreach ($payments as $payment)
           <tr>
-            <td>: {{ $payment->created_at }}</td>
-            <td>: {{ $payment->paidAmount['value'] }}</td>
+            <td>VA Number</td>
+            <td>: {{ $va->number }}</td>
           </tr>
-          @endforeach
-        @endif
-      </tbody>
-    </table>
-    <x-forms.button href="{{ route('va.index') }}" preset="default">{{ __('Back') }}</x-forms.button>
-  </x-containers.card>
+        </table>
+        <x-forms.button href="{{ route('va.index') }}" preset="default">{{ __('Back') }}</x-forms.button>
+      </x-containers.card>
+    </div>
+    <div class="col-md-6">
+      <x-containers.card>
+        <x-slot name="title">History Transaksi</x-slot>
+        <table class="table">
+          <thead>
+            <th>Waktu</th>
+            <th>Total Pembayaran</th>
+          </thead>
+          <tbody>
+            @if (!isset($payments) || sizeof($payments) <= 0)
+              <tr>
+                <td colspan="2">Belum ada history</td>
+              </tr>
+            @else
+              @foreach ($payments as $payment)
+              <tr>
+                <td>: {{ $payment->created_at }}</td>
+                <td>: {{ json_decode($payment->paidAmount)['value'] }}</td>
+              </tr>
+              @endforeach
+            @endif
+          </tbody>
+        </table>
+        <x-forms.button href="{{ route('va.index') }}" preset="default">{{ __('Back') }}</x-forms.button>
+      </x-containers.card>
+    </div>
+  </div>
 </x-containers.container>
 @endsection
 
