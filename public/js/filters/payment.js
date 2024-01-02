a471f48d08bfb955ec5699067e78d417f43ba1c0
Maspri <priyono.arif@gmail.com>
2024-01-02 22:16:10 +0700

    fix: missing element id

--- public/js/filters/payment.js
+++ public/js/filters/payment.js
@@ -1,23 +1,27 @@
 const statusFilter = document.getElementById('filter-status');
-statusFilter.addEventListener('change', ({ target: { value }}) => {
-    const url = new URL(window.location.href);
-    if (value) {
-        url.searchParams.set('status', value);
-    } else {
-        url.searchParams.delete('status');
-    }
-    url.searchParams.delete('page');
-    window.location.href = url
-})
+if (statusFilter) {
+    statusFilter.addEventListener('change', ({ target: { value }}) => {
+        const url = new URL(window.location.href);
+        if (value) {
+            url.searchParams.set('status', value);
+        } else {
+            url.searchParams.delete('status');
+        }
+        url.searchParams.delete('page');
+        window.location.href = url
+    })
+}
 
 const periodFilter = document.getElementById('filter-period');
-periodFilter.addEventListener('change', ({ target: { value }}) => {
-    const url = new URL(window.location.href);
-    if (value) {
-        url.searchParams.set('period', value);
-    } else {
-        url.searchParams.delete('period');
-    }
-    url.searchParams.delete('page');
-    window.location.href = url
-})
+if (periodFilter) {
+    periodFilter.addEventListener('change', ({ target: { value }}) => {
+        const url = new URL(window.location.href);
+        if (value) {
+            url.searchParams.set('period', value);
+        } else {
+            url.searchParams.delete('period');
+        }
+        url.searchParams.delete('page');
+        window.location.href = url
+    })
+}
